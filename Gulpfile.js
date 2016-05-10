eb4ce61cd017f07e3c8df329edfcbfc3c83a1adc
Tom Kruijsen <tom@tomkruijsen.nl>
2016-05-10 21:18:28 +0200

    Add simple server task

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var handlebars = require('handlebars');
 var gulpHandlebars = require('gulp-handlebars-html')(handlebars);
 var rename = require('gulp-rename');
+const path = require('path');
+const connect = require('gulp-connect');
 const post = require('./tasks/post.js');
 
 // Builds the site into the `build` folder.
@@ -12,11 +14,12 @@ gulp.task('build', function() {
     .pipe(gulp.dest('build'));
 });
 
+//Build all the post pages.
 gulp.task('buildPosts', () => {
   gulp.src('posts/*.md')
     .pipe(post.build)
     .pipe(rename({extname: '.html'}))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest(path.join('build', 'dev')));
 });
 
 // Puts a draft in the posts folder.
@@ -29,4 +32,7 @@ gulp.task('post', function(title) {
 
 // Serves the build folder.
 gulp.task('server', function(title) {
+  connect.server({
+    root: 'build/dev'
+  });
 });
